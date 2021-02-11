e1778d1697ff272cc9fcd0fbe3cb1d30b5ce8968
Lincoln Nguyen <letsmakeaworld@icloud.com>
2021-02-10 19:39:29 -0600

    Retweeting done

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -33,11 +33,26 @@ class TweetCellTableViewCell: UITableViewCell {
     }
     
     @IBAction func retweet(_ sender: Any) {
-        
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (Error) in
+            print("Error in retweeting: \(Error)")
+        })
+    }
+    
+    func setRetweeted(_ isRetweeted:Bool) {
+        if (isRetweeted) {
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        } else {
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
     }
     
     var favorited:Bool = false
     var tweetId:Int = -1
+    // var retweeted:Bool = false
     
     func setFavorite(_ isFavorited:Bool) {
         favorited = isFavorited
